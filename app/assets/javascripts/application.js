d9aa62bc060fa61ecf71dd89c9fa31b89413c123
Caleb Phillips <caleb@virgil.smallwhitecube.com>
2013-01-26 16:12:46 -0700

    fancy features to use location when possible

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require prototype
+//= require prototype_ujs
+//= require effects
+//= require dragdrop
+//= require controls
 //= require_tree .
