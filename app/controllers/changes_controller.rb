7ec1b83b2de94bbf99ef393a2272b72c821689c8
Caleb <caleb@krige.(none)>
2013-04-28 16:24:47 -0700

    Limit change log output!

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -3,7 +3,7 @@ class ChangesController < ApplicationController
   # GET /types
   # GET /types.json
   def index
-    @changes = Change.find(:all,:order => "created_at DESC")
+    @changes = Change.find(:all,:order => "created_at DESC", :limit => 100)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @changes }
