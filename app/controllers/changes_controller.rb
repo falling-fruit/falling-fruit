4b4a2b56361c05a1cf5b7f92f650df79107e28b1
Caleb <caleb@krige.(none)>
2013-05-09 08:01:54 -0700

    Okay, cool. Changes is fast now.

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -3,7 +3,12 @@ class ChangesController < ApplicationController
   # GET /types
   # GET /types.json
   def index
-    @changes = Change.select("extract(days from (NOW()-created_at)) as days_ago, *").find(:all,:order => "created_at DESC", :limit => 100)
+    r = ActiveRecord::Base.connection.execute("SELECT string_agg(coalesce(t.name,lt.type_other),',') as type_title,
+      extract(days from (NOW()-c.created_at)) as days_ago, c.location_id, c.admin_id, c.description, c.remote_ip
+      FROM changes c, locations l, locations_types lt left outer join types t on lt.type_id=t.id
+      WHERE lt.location_id=l.id AND l.id=c.location_id
+      GROUP BY l.id, c.location_id, c.admin_id, c.description, c.remote_ip, c.created_at ORDER BY c.created_at DESC LIMIT 100");
+    @changes = r.collect{ |row| row }
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @changes }
