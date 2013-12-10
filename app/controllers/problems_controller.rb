751477fba5d4d3402ebdfd43bba21af4c4e0f243
caleb <caleb@zeropercent.us>
2013-12-09 21:53:32 -0600

    Fix some problems with problems ;)

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -19,7 +19,7 @@ class ProblemsController < ApplicationController
       test = user_signed_in? ? true : verify_recaptcha(:model => @problem, 
                                                        :message => "ReCAPCHA error!")
       if test and @problem.save
-        format.html { redirect_to @problem.location, notice: 'Problem was successfully created.' }
+        format.html { render :text => 'Thanks for letting us know!' }
       else
         format.html { render :partial => "/problems/new", :locals => {:problem => @problem} }
       end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,6 +4,8 @@ class Problem < ActiveRecord::Base
   belongs_to :location
   attr_accessible :comment, :problem_code, :reporter_id, :resolution_code, :responder_id, :response, :reporter, :responder, :id, :location_id, :name, :email
 
+  validates :problem_code, :numericality => { :only_integer => true }, :allow_nil => false
+
   Codes = ["This Location is SPAM. Please delete.",
            "This Location is wrong or doesn't exist. Please delete.",
            "This Location is a duplicate. Please delete.",
