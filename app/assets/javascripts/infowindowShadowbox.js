40429aec909d1b4fc86bd8ee3a8999854b2ff83d
Ethan Welty <ethan.welty@gmail.com>
2013-12-13 17:53:40 -0700

    Click to next in shadowbox.

--- app/assets/javascripts/infowindowShadowbox.js
+++ app/assets/javascripts/infowindowShadowbox.js
@@ -1,14 +1,30 @@
-	Shadowbox.init({
-	 	language: 'en', 
-    players: ['img'], 
-		skipSetup: true,
-		cache: false,
-		animate: false,
-		animateFade: false,
-		showOverlay: true,
-		overlayOpacity: 0.6,
-		counterType: "default",
-		continuous: true,
-		displayCounter: true,
-
-	});
+Shadowbox.init({
+  language: 'en', 
+  players: ['img'], 
+  skipSetup: true,
+  cache: false,
+  animate: false,
+  animateFade: false,
+  showOverlay: true,
+  overlayOpacity: 0.6,
+  counterType: "default",
+  continuous: true,
+  displayCounter: true,
+  // Prevent arrows from switching photos and panning map
+  onOpen: function() {
+    if (typeof map == "object" & typeof map.setOptions == "function") map.setOptions({keyboardShortcuts: false})
+    var nextLink = $('<a>').attr('id', 'sb-custom-nav-next');
+    if (Shadowbox.hasNext()) {
+      nextLink.bind('click', function(event) {Shadowbox.next();});
+    } else {
+      nextLink.bind('click', function(event) {Shadowbox.close();});
+    }
+    $('#sb-body').append(nextLink);
+    // previous currently unused
+    // sb-custom-nav-previous
+  },
+  onClose: function() {
+    if (typeof map == "object" & typeof map.setOptions == "function") map.setOptions({keyboardShortcuts: true})
+    $('#sb-custom-nav-next').remove();
+  },
+});
