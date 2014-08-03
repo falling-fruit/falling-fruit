f468174fa627c5de92c5d881107f7e301465047c
caleb <caleb@zeropercent.us>
2014-08-03 16:36:56 -0700

    more progress on i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,22 +3,22 @@ module ApplicationHelper
   def faster_time_ago_in_words(t)
     days = (Time.now-t)/(60*60*24)
     if days < 1
-      "less than 24 hours"
+      I18n.t("helpers.faster_time_ago_in_words.less_than_24_hours", :default => "less than 24 hours")
     elsif days < 31
-      "#{days.ceil} days"
+      "#{days.ceil} " + I18n.t("helpers.faster_time_ago_in_words.days", :default => "days")
     elsif days < 365
       months = days/31
       if months == 1
-        "last month"
+        I18n.t("helpers.faster_time_ago_in_words.last_month", :default => "last month")
       else
-        "#{months.floor} months ago"
+        "#{months.floor} " + I18n.t("helpers.faster_time_ago_in_words.months_ago",:default => "months ago")
       end
     elsif
       years = days/365
       if years == 1
-        "last year"
+        I18n.t("helpers.faster_time_ago_in_words.last_year",:default => "last year")
       else
-        "#{years.floor} years ago"
+        "#{years.floor} " + I18n.t("helpers.faster_time_ago_in_words.years_ago", :default => "years ago")
       end
     end
   end
