b8fef5ed012d6791fc936671ddf3c60b717da175
caleb <caleb@zeropercent.us>
2014-08-15 09:11:22 -0700

    Fix empty review issue. Allow blank categories

--- lib/bitmask_stuff.rb
+++ lib/bitmask_stuff.rb
@@ -1,13 +1,15 @@
 module BitmaskStuff
 
   def mask_to_array(mask,options)
+    return [] if mask.nil? or mask == 0
     options.each_with_index.collect{ |v,i| (mask & 1<<i) > 0 ? v : nil }.compact
   end
   module_function :mask_to_array
 
   def array_to_mask(values,options)
+    return 0 if values.nil? or (values.kind_of? Array and values.empty?)
     r = 0
-    options.each_with_index.each{ |v,i| r = r | 1<<i unless values.index(v).nil? }
+    options.each_with_index.each{ |v,i| r = r | 1<<i unless vals.index(v).nil? }
     r
   end
   module_function :array_to_mask
