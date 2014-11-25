84a368a45a2771d4c1a6e1ef208c3478293f6138
Ubuntu <ubuntu@smallwhitecube.com>
2014-11-25 02:58:38 +0000

    Date-tagged backuping

--- db/backup.sh
+++ db/backup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-num_pg_backups=30
+num_backups=30
 backup_dir="backups"
 prefix="fallingfruit"
+ymd=$(date +%Ymd)
 
+pg_dump -h localhost -U ${prefix}_user -Fc -b -v -f $prefix.$ymd.sql ${prefix}_new_db
 pushd $backup_dir
-for i in $(seq $num_pg_backups -1 1);do
-  if [ -f $prefix.$i.sql ];then
-    echo "Rolling backup $i to $(($i+1))"
-    mv $prefix.$i.sql $prefix.$(($i+1)).sql
+n=0
+for i in $(ls -1 *.sql | sort -rn);do
+  n=$(($n + 1))
+  if [ $n -gt $num_backups ];then
+    rm $i
   fi
 done
-rm -f $prefix.$(($num_pg_backups+1)).sql
-pg_dump -h localhost -U fallingfruit_user -Fc -b -v -f $prefix.1.sql fallingfruit_new_db
 popd
