6e750163ff13b8afdfb92a9b763b255b5016b207
Ubuntu <ubuntu@smallwhitecube.com>
2014-12-26 22:42:48 +0000

    Fix backup script!

--- db/backup.sh
+++ db/backup.sh
@@ -3,10 +3,12 @@
 num_backups=30
 backup_dir="backups"
 prefix="fallingfruit"
-ymd=$(date +%Ymd)
+ymd=$(date +%Y%m%d)
 
-pg_dump -h localhost -U ${prefix}_user -Fc -b -v -f $prefix.$ymd.sql ${prefix}_new_db
+pg_dump -h localhost -U ${prefix}_user -Fc -b -v -f $backup_dir/$prefix.$ymd.sql ${prefix}_new_db
 pushd $backup_dir
+rm $prefix.latest.sql
+ln $prefix.$ymd.sql $prefix.latest.sql
 n=0
 for i in $(ls -1 *.sql | sort -rn);do
   n=$(($n + 1))
