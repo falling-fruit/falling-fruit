e4c94d2b768c42d78e81da2b29d2f5ab4e9e209f
ezwelty <ethan.welty@gmail.com>
2015-03-07 15:52:48 -0700

    Devise emails now use https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ FallingfruitWebapp::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'fallingfruit.org' }
+  config.action_mailer.default_url_options = { :host => 'fallingfruit.org', :protocol => 'https' }
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
