90d30f4801cc3da892f82f47378ced2df0194333
Ethan Welty <ethan.welty@gmail.com>
2015-03-09 17:05:59 -0600

    https for devise mailer in all environments, https press update, sharing update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,7 @@ FallingfruitWebapp::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'fallingfruit.org' }
+  config.action_mailer.default_url_options = { :host => 'fallingfruit.org', :protocol => 'https' }
   config.action_mailer.perform_deliveries = false
 
   # Raise exception on mass assignment protection for Active Record models
