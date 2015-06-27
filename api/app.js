982e2f1b6da13f9e5c0d0c56b42749f0aa201027
Caleb <cphillips@smallwhitecube.com>
2015-06-27 13:09:12 -0600

    clusters.json implemented!

--- api/app.js
+++ api/app.js
@@ -63,8 +63,17 @@ function check_api_key(req,client,callback){
   });
 }
 
-
 // Routes
+
+// TODO:
+
+// PUT /locations/:id.json (edit location) - 0.2
+// POST /locations.json (add location) - 0.2
+// POST /locations/:id/reviews.json (add review) - 0.2
+// GET /locations/mine.json - 0.3
+
+// Note: /locations/marker.json is now obsolete (covered by /locations/:id.json)
+// Note: /locations/nearby.json is now obsolete (covered by /locations.json)
 
 // Note: grid parameter replaced by zoom
 // Note: now can accept a bounding box, obviating the cluster_types.json endpoint
@@ -97,7 +106,7 @@ app.get('/types.json', apicache('1 hour'), function (req, res) {
                         AND (category_mask & $1)>0 "+filters+" GROUP BY t.id, name, scientific_name \
                         ORDER BY name,scientific_name;",
                        [cmask],function(err, result) {
-            if (err) callback(err,'error running query');
+            if (err) return callback(err,'error running query');
             res.send(__.map(result.rows,function(x){ 
               x.count = parseInt(x.count); 
               return x; 
@@ -107,7 +116,7 @@ app.get('/types.json', apicache('1 hour'), function (req, res) {
           client.query("SELECT id, COALESCE("+name+",name) as name,scientific_name FROM types WHERE NOT \
                         pending AND (category_mask & $1)>0 ORDER BY name,scientific_name;",
                        [cmask],function(err, result) {
-            if (err) callback(err,'error running query');
+            if (err) return callback(err,'error running query');
             res.send(result.rows);
           });
         }
@@ -185,8 +194,63 @@ app.get('/locations.json', function (req, res) {
                       WHERE t.id=ANY(l.type_ids) "+filters+" GROUP BY l.id, l.lat, l.lng, l.unverified \
                       HAVING "+cfilter+" ORDER BY sort LIMIT $1 OFFSET $2;",
                      [limit,offset],function(err, result) {
-          if (err) callback(err,'error running query');
-          res.send([result.rowCount,total_count].concat(result.rows));
+          if (err) return callback(err,'error running query');
+          res.send([result.rowCount,total_count].concat(__.map(result.rows,function(x){
+            if(x.num_reviews) x.num_reviews = parseInt(x.num_reviews);
+            return x;
+          })));
+        });
+      }
+    ],
+    function(err,message){
+      done();
+      if(message) send_error(res,message,err);
+    }); 
+  });
+});
+
+// Note: grid param renamed to zoom
+// Note: does not implicitly include children, we leave that to the client
+// Note: does not return title, client is responsible for formatting (i.e., calling number_to_human)
+app.get('/clusters.json', function (req, res) {
+  var cmask = default_catmask;
+  if(req.query.c) cmask = catmask(req.query.c.split(",")); 
+  var cfilter = "(bit_or(t.category_mask) & "+cmask+")>0";
+  var mfilter = "";
+  if(req.query.muni == 0) mfilter = "AND NOT muni";
+  var bfilter = undefined;
+  var zfilter = "zoom=2"; // zfilter is first so doesn't have an AND
+  if(__.every([req.query.swlat,req.query.swlng,req.query.nelat,req.query.nelng])){
+    bfilter = postgis_bbox("cluster_point",parseFloat(req.query.nelat),parseFloat(req.query.nelng),
+                           parseFloat(req.query.swlat),parseFloat(req.query.swlng),900913);
+    if(req.query.zoom) zfilter = "zoom="+parseInt(req.query.zoom);
+  }else{
+    return send_error(res,'bounding box not defined');    
+  }
+  var tfilter = "AND type_id IS NULL";
+  if(req.query.t){
+    var tids = __.map(req.query.t.split(","),function(x){ parseInt(x) });
+    tfilter = "AND type_id IN ("+tids.join(",")+")";
+  }
+  var filters = __.reject([zfilter,tfilter,bfilter,mfilter],__.isUndefined).join(" ");
+  db.pg.connect(db.conString, function(err, client, done) {
+    if (err){ 
+      send_error(res,'error fetching client from pool',err);
+      return done();
+    }
+    async.waterfall([
+      function(callback){ check_api_key(req,client,callback) },
+      function(callback){
+        client.query("SELECT ST_X(center_point) AS center_x, ST_Y(center_point) AS center_y, count FROM \
+                      (SELECT ST_Transform(ST_SetSRID(ST_POINT(SUM(count*ST_X(cluster_point))/SUM(count), \
+                      SUM(count*ST_Y(cluster_point))/SUM(count)),900913),4326) as center_point, \
+                      SUM(count) as count FROM clusters WHERE "+filters+" GROUP BY grid_point) subq;",
+                     [],function(err, result) {
+          if (err) return callback(err,'error running query');
+          res.send(__.map(result.rows,function(x){
+            x.count = parseInt(x.count);
+            return x;
+          }));
         });
       }
     ],
@@ -225,7 +289,7 @@ app.get('/locations/:id(\\d+).json', function (req, res) {
           client.query("SELECT photo_updated_at, photo_file_name FROM observations \
                         WHERE photo_file_name IS NOT NULL AND location_id=$1;",
                        [id],function(err, result) {
-            if (err) callback(err,'error running query');
+            if (err) return callback(err,'error running query');
             location.photos = result.rows;
             res.send(location);
           });
@@ -255,7 +319,7 @@ app.get('/locations/:id(\\d+)/reviews.json', function (req, res) {
                       photo_file_name, fruiting, quality_rating, yield_rating, author, photo_caption \
                       FROM observations WHERE location_id=$1;",
                      [id],function(err, result) {
-          if (err) callback(err,'error running query');
+          if (err) return callback(err,'error running query');
           res.send(result.rows);
         });
       }
@@ -266,8 +330,6 @@ app.get('/locations/:id(\\d+)/reviews.json', function (req, res) {
     });    
   });
 });
-
-// TODO:
 
 //  def mine
 //    return unless check_api_key!("api/locations/mine")
@@ -326,107 +388,6 @@ app.get('/locations/:id(\\d+)/reviews.json', function (req, res) {
 //    end
 //  end
 //  
-
-//  def cluster
-//    return unless check_api_key!("api/locations/cluster")
-//    # Muni API is locked to muni & forager
-//    if !@api_key.nil? and @api_key.api_type == "muni"
-//      params[:muni] = 1
-//      params[:c] = "forager"
-//    end
-//    mfilter = ""
-//    if params[:muni].present? and params[:muni].to_i == 1
-//      mfilter = ""
-//    elsif params[:muni].present? and params[:muni].to_i == 0
-//      mfilter = "AND NOT muni"
-//    end
-//    tfilter = "AND type_id IS NULL"
-//    if params[:t].present?
-//      type = Type.find(params[:t])
-//      tids = ([type.id] + type.all_children.collect{ |c| c.id }).compact.uniq
-//      tfilter = "AND type_id IN (#{tids.join(",")})"
-//    end
-//    g = params[:grid].present? ? params[:grid].to_i : 2
-//    g = 12 if g > 12
-//    if [params[:nelat],params[:nelng],params[:swlat],params[:swlng]].any? { |e| e.nil? }
-//      bound = ""
-//    elsif params[:swlng].to_f < params[:nelng].to_f
-//      bound = "AND ST_INTERSECTS(polygon,ST_TRANSFORM(ST_SETSRID(ST_MakeBox2D(ST_POINT(#{params[:swlng]},#{params[:swlat]}), ST_POINT(#{params[:nelng]},#{params[:nelat]})),4326),900913))"
-//    else # map spans -180 | 180 seam, split into two polygons
-//      bound = "AND (ST_INTERSECTS(polygon,ST_TRANSFORM(ST_SETSRID(ST_MakeBox2D(ST_POINT(-180,#{params[:swlat]}), ST_POINT(#{params[:nelng]},#{params[:nelat]})),4326),900913)) OR ST_INTERSECTS(cluster_point,ST_TRANSFORM(ST_SETSRID(ST_MakeBox2D(ST_POINT(#{params[:swlng]},#{params[:swlat]}), ST_POINT(180,#{params[:nelat]})),4326),900913)))"
-//    end
-//    
-//    @clusters = Cluster.select("SUM(count*ST_X(cluster_point))/SUM(count) as center_x,
-//                                SUM(count*ST_Y(cluster_point))/SUM(count) as center_y,
-//                                SUM(count) as count").group("grid_point").where("zoom = #{g} #{mfilter} #{tfilter} #{bound}")
-//                                
-//    earth_radius = 6378137.0
-//    earth_circum = 2.0*Math::PI*earth_radius
-//    
-//    # Conversion SRID 4326 <-> 900913
-//    # x = (lng/360)*earth_circum
-//    # lng = x*(360/earth_circum)
-//    # y = Math.log(Math.tan((lat+90)*(Math::PI/360)))*earth_radius
-//    # lat = 90-(Math.atan2(1,Math.exp(y/earth_radius))*(360/Math::PI))
-//    
-//    # FIXME: calc pixel distances between cluster positions, merge as necessary
-//    @clusters.collect!{ |c|
-//      v = {}
-//      
-//      # make single cluster at z = 0 snap to middle of map (optional)
-//      if g == 0
-//        v[:lat] = 0
-//        v[:lng] = 0
-//      else      
-//        v[:lat] = 90-(Math.atan2(1,Math.exp(c.center_y.to_f/earth_radius))*(360/Math::PI))
-//        v[:lng] = c.center_x.to_f*(360/earth_circum)
-//      end
-//      v[:n] = c.count
-//      v[:title] = number_to_human(c.count)
-//      v
-//    }
-//    log_api_request("api/locations/cluster",@clusters.length)
-//    respond_to do |format|
-//      format.json { render json: @clusters }
-//    end
-//  end
-
-
-//  def marker
-//    return unless check_api_key!("api/locations/marker")
-//     id = params[:id].to_i
-//     i18n_name_field = I18n.locale != :en ? "t.#{I18n.locale.to_s.tr("-","_")}_name," : ""
-//     r = ActiveRecord::Base.connection.execute("SELECT l.id, l.lat, l.lng, l.unverified, array_agg(t.id) as types,
-//      array_agg(t.parent_id) as parent_types,
-//      string_agg(coalesce(#{i18n_name_field}t.name),',') as name    
-//      FROM locations l, types t
-//      WHERE t.id=ANY(l.type_ids) AND l.id=#{id}
-//      GROUP BY l.id, l.lat, l.lng, l.unverified")
-//    @markers = r.collect{ |row|
-//      row["parent_types"] = row["parent_types"].tr('{}','').split(/,/).reject{ |x| x == "NULL" }.collect{ |e| e.to_i }
-//      row["types"] = row["types"].tr('{}','').split(/,/).reject{ |x| x == "NULL" }.collect{ |e| e.to_i }
-//      if row["name"].nil? or row["name"].strip == ""
-//        name = "Unknown"
-//      else
-//        t = row["name"].split(/,/)
-//        if t.length == 2
-//          name = "#{t[0]} & #{t[1]}"
-//        elsif t.length > 2
-//          name = "#{t[0]} & Others"
-//        else
-//          name = t[0]
-//        end
-//      end
-//      {:title => name, :location_id => row["id"], :lat => row["lat"], :lng => row["lng"], 
-//       :parent_types => row["parent_types"],:n => 1, :types => row["types"]}
-//    } unless r.nil?
-//    log_api_request("api/locations/marker",1)
-//    respond_to do |format|
-//      format.json { render json: @markers }
-//    end
-//  end
-
-//end
 
 var server = app.listen(3100, function () {
 
