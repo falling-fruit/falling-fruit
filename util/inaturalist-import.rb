cf7423054b921668af5c13b1783838fe5f53b50f
Caleb Phillips <caleb.phillips@nrel.gov>
2016-04-25 15:18:25 -0600

    Seems to be working

--- util/inaturalist-import.rb
+++ util/inaturalist-import.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
-require '../../config/environment'
+require '../config/environment'
 
 require 'open-uri'
 require 'json'
 
 Endpoint="http://api.inaturalist.org/v1/observations?project_id=edible-flora&order=desc&order_by=created_at"
+PerPage=50
 json = JSON.load(open(Endpoint))
 
 import_name = "iNaturalist - Edible Flora Project"
@@ -15,44 +16,63 @@ if i.empty?
   i.url = "https://www.inaturalist.org/projects/edible-flora"
   i.name = import_name
   i.comments = "Edible flora imported from the iNaturalist API"
+  i.save
 else
   i = i.first
 end
 
-ids = Location.select("inaturalist_id").where("inaturalist_id IS NOT NULL")
+ids = Location.select("inaturalist_id").where("inaturalist_id IS NOT NULL").collect{ |x| x.inaturalist_id }
+puts ids
 
-json["results"].each{ |result|
-  print id
-  id = result["id"]
-  if ids.include? id
-    puts "...done"
-    next
-  end
+page = 1
+todo = nil
+while todo.nil? or ((page-1)*PerPage < todo)
+  puts "page #{page}"
+  json = JSON.load(open(Endpoint + "&per_page=#{PerPage}&page=#{page}"))
+  todo = json["total_results"]
+  page = json["page"] + 1
+  json["results"].each{ |result|
+    id = result["id"]
+    print id
+    if ids.include? id
+      puts "...done"
+      next
+    end
 
-  l = Location.new
-  o = Observation.new
-  o.observed_on = result["observed_on_details"]["date"]
-  
-  species_guess = result["species_guess"]
-  species_scientific = result["taxon"]["name"]
-  species_common = result["taxon"]["preferred_common_name"]
+    l = Location.new
+    o = Observation.new
+    o.observed_on = result["observed_on_details"]["date"] unless result["observed_on_details"].nil?
+ 
+    tids = [] 
+    species_guess = result["species_guess"]
+    unless result["taxon"].nil?
+      species_scientific = result["taxon"]["name"]
+      species_common = result["taxon"]["preferred_common_name"]
+    end
+    tids = Type.select("id").where("scientific_name ILIKE ?","%#{species_scientific}%").collect{ |r| r.id } unless result["taxon"].nil?
+    tids = Type.select("id").where("name ILIKE ?","%#{species_common}%").collect{ |r| r.id } if tids.empty? unless result["taxon"].nil?
+    tids = Type.select("id").where("name ILIKE ?","%#{species_guess}%").collect{ |r| r.id } if tids.empty?
 
-  tids = Type.select("id").where("scientific_name ILIKE ?","%#{species_scientific}%").collect{ |r| r.id }
-  tids = Type.select("id").where("name ILIKE ?","%#{species_common}%").collect{ |r| r.id } if tids.empty?
-  tids = Type.select("id").where("name ILIKE ?","%#{species_guess}%").collect{ |r| r.id } if tids.empty?
+    if tids.empty?
+      puts "...no type"
+      next
+    end
 
-  if tids.empty?
-    puts "...no type"
-    next
-  end
+    next if result["geojson"].nil? or result["geojson"]["coordinates"].nil?
+    l.lng = result["geojson"]["coordinates"][0]
+    l.lat = result["geojson"]["coordinates"][1]
+    l.author = result["user"]["login"]
+    o.author = l.author
+    l.description = result["uri"]
+    l.type_ids = [tids.first]
+    l.import_id = i.id
+    l.inaturalist_id = id
 
-  l.lng = result["geojson"]["coordinates"][0]
-  l.lat = result["geojson"]["coordinates"][1]
-  l.author = result["user"]["login"]
-  l.description = result["uri"]
-  l.type_ids << tids.first
+    l.observations << o
 
-  l.observations << o
+    l.save
+    o.save
 
-#  l.save
-}
+    puts "...okay (type = #{tids.first}, location = #{l.id})"
+  }
+end
