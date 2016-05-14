fc312dc3c3815635aa2a4f89ac1559874a4e0846
ezwelty <ethan.welty@gmail.com>
2016-05-14 17:17:22 +0200

    Added el_name to accessible fields

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,24 +1,24 @@
 class Type < ActiveRecord::Base
 
-  attr_accessible :name, :synonyms, 
-                  :fr_name, :de_name, :es_name, :pt_br_name, :it_name, :pl_name, :he_name,
+  attr_accessible :name, :synonyms,
+                  :fr_name, :de_name, :es_name, :pt_br_name, :it_name, :pl_name, :he_name, :el_name,
                   :scientific_name, :scientific_synonyms, :taxonomic_rank,
-                  :usda_symbol, :wikipedia_url, 
+                  :usda_symbol, :wikipedia_url,
                   :urban_mushrooms_url, :fruitipedia_url, :foraging_texas_url, :eat_the_weeds_url,
-                  :edability, :category_mask, :pending, 
-                  :parent_id, :parent, 
+                  :edability, :category_mask, :pending,
+                  :parent_id, :parent,
                   :marker, :notes
   has_attached_file :marker
   belongs_to :parent, class_name: "Type"
   has_many :children, class_name: "Type", foreign_key: "parent_id"
   has_many :invasives
-  
+
   normalize_attributes *character_column_symbols
   normalize_attribute :name, :before => [ :squish ] do |value|
     value.is_a?(String) ? value.gsub(/[^[:word:]\s\(\)\-\']/,'') : value
   end
   validates :name, :presence => true
-  
+
   Ranks={0 => "Polyphyletic", 1 => "Kingdom", 2 => "Phylum", 3 => "Class", 4 => "Order", 5 => "Family",
          6 => "Genus", 7 => "Multispecies", 8 => "Species", 9 => "Subspecies"}
   Edabilities={-1 => "Not worth it (or toxic)", 1 => "Include", 2 => "Maybe include"}
@@ -83,7 +83,7 @@ class Type < ActiveRecord::Base
     ret["children"] = cs.collect{ |c| c.to_hash(cats) }.compact unless cs.empty?
     ret
   end
-  
+
   # Type filter 2.0 (forthcoming)
   def Type.dyna_tree(cats=DefaultCategories)
     cat_mask = array_to_mask(cats,Categories)
@@ -92,7 +92,7 @@ class Type < ActiveRecord::Base
       Type.where("NOT pending AND parent_id is NULL AND (category_mask & ?)>0",cat_mask).default_sort.collect{ |t| t.to_dyna(cats) }
     end
   end
-  
+
   def to_dyna(cats=DefaultCategories)
     cat_mask = array_to_mask(cats,Categories)
     $seen = {} if $seen.nil?
@@ -100,7 +100,7 @@ class Type < ActiveRecord::Base
     $seen[self.id] = true
     cs = self.children.where("(category_mask & ?)>0",cat_mask).default_sort
     if cs.empty?
-      ret = {"key" => self.id, "title" => self.full_name}  
+      ret = {"key" => self.id, "title" => self.full_name}
     else
       ret = {"key" => self.id.to_s + ".all", "title" => self.full_name}
       ret["children"] = cs.collect{ |c| c.to_dyna(cats) }.compact
@@ -108,7 +108,7 @@ class Type < ActiveRecord::Base
     end
     ret
   end
-  
+
   # Location types
   def Type.full_list(cats = DefaultCategories)
     cat_mask = array_to_mask(cats,Categories)
@@ -130,17 +130,17 @@ class Type < ActiveRecord::Base
       select("array_to_string(ARRAY[parents_types.name,types.name],'::') as sortme, parents_types.name as parent_name, types.*").
       order(:sortme)
   end
-  
+
   # Default sorting scheme
   def Type.default_sort
   	#Type.order('scientific_name ASC NULLS LAST, taxonomic_rank ASC').sort_by{ |t| t.scientific_name.blank? ? t.i18n_name : '' }
     self.select("*, COALESCE(" + Type.i18n_name_field + ", name) as i18n_name_sql").order("scientific_name ASC NULLS LAST, taxonomic_rank, i18n_name_sql")
   end
-  
+
   def locations
     Location.where("type_ids @> ARRAY[?]",self.id)
   end
-  
+
   def children_ids
     self.children.collect{ |t| t.id }
   end
