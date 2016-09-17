33bf45ab43ec1085b0e38a789987ec09f935a45c
ezwelty <ethan.welty@gmail.com>
2016-09-17 10:50:36 -0600

    Fix confirmation instructions email

--- app/mailers/devise_custom.rb
+++ app/mailers/devise_custom.rb
@@ -1,9 +1,10 @@
-class DeviseCustom < Devise::Mailer   
+class DeviseCustom < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  # default template_path: 'devise_cusom'
 
   def confirmation_instructions(record)
     headers['X-SMTPAPI'] = '{"category": "FF-Rails-DeviseConfirmation"}'
-    devise_mail(record, :confirmation_instructions)
+    super
   end
 end
