80a4c3620e2c7d7bee1a73e91d8bbd2bb375f0a5
Ethan Welty <ethan.welty@gmail.com>
2016-11-30 18:02:08 -0700

    Attempt at a speedier user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @announcements_list = User.select("email").where("announcements_email AND confirmed_at IS NOT NULL").
-      collect{ |u| u.email }
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
