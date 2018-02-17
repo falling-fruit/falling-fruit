fecc9364d27d35e953f35c6d62b9c9e0b9912a4d
ezwelty <ethan.welty@gmail.com>
2018-02-17 15:49:18 -0700

    Fix the validation of new types

--- app/models/type.rb
+++ app/models/type.rb
@@ -18,8 +18,8 @@ class Type < ActiveRecord::Base
   validate :en_or_scientific_present?
 
   def en_or_scientific_present?
-    if %w(en_name, scientific_name).all?{|attr| self[attr].blank?}
-      errors.add :base, "en_name and scientific name cannot both be missing"
+    if en_name.blank? and scientific_name.blank?
+      errors.add(:base, "en_name and scientific name cannot both be missing")
     end
   end
 
