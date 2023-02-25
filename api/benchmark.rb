3066721faa6257029518ddeaaf4e5193759a0c60
ezwelty <ethan.welty@gmail.com>
2023-02-25 15:14:30 +0100

    Update API benchmarking script
    * Add v0.3 API
    * Select bounds based on location coordinates
    * Adjust queries to be more comparable

--- api/benchmark.rb
+++ api/benchmark.rb
@@ -3,65 +3,54 @@
 require 'benchmark'
 require 'net/http'
 require 'csv'
-require 'base64'
-require 'active_support/core_ext/hash/indifferent_access'
 
-LatMin = -85.0
-LatMax = 85.0
-LngMin = -180.0
-LngMax = 180.0
-N = 1000
+WHICH = ARGV[0]
+CSV_PATH = ARGV[1]
+DEBUG = ARGV[2]
 
-Which = ARGV[0]
-ParamsCSV = ARGV[1]
-
-def rand_box
-  swlng = (LngMax-LngMin)*rand + LngMin
-  swlat = (LatMax-LatMin)*rand + LatMin
-  nelng = (LngMax-swlng)*rand + swlng
-  nelat = (LatMax-swlat)*rand + swlat
-  return "nelat=#{nelat}&nelng=#{nelng}&swlat=#{swlat}&swlng=#{swlng}"
+def point_to_box(lat, lng, width)
+  width = width.to_f
+  return [
+    [lat - width / 2, -85].max,
+    [lng - width / 2, -180].max,
+    [lat + width / 2, 85].min,
+    [lng + width / 2, 180].min
+  ]
 end
 
-def params_box(p)
-  return "nelat=#{p[:nelat]}&nelng=#{p[:nelng]}&swlat=#{p[:swlat]}&swlng=#{p[:swlng]}"
+def box_to_old_bounds(box)
+  return "swlat=#{box[0]}&swlng=#{box[1]}&nelat=#{box[2]}&nelng=#{box[3]}"
 end
 
-puts "old new"
-
-unless ParamsCSV.nil?
-  n = 0
-  params = []
-  CSV.foreach(ParamsCSV) do |row|
-    n += 1
-    next if n == 1
-    p = Marshal.load(Base64.decode64(row[0]))
-    params.push(p) if p[:nelat]
-  end
-  params.shuffle!
+def box_to_new_bounds(box)
+  return "bounds=#{box[0]},#{box[1]}%7C#{box[2]},#{box[3]}"
 end
 
-(1..N).each{ |i|
-  unless ParamsCSV.nil?
-    if Which == "cluster"
-      url_old = URI("http://fallingfruit.org/api/locations/cluster.json?#{params_box(params[i])}&grid=#{params[i][:grid]}&api_key=BJBNKMWM")
-      url_new = URI("http://fallingfruit.org/api/0.2/clusters.json?#{params_box(params[N+i])}&zoom=#{params[N+i][:grid]}&api_key=BJBNKMWM")
-    else
-      url_old = URI("http://fallingfruit.org/api/locations/cluster.json?#{rand_box}&api_key=BJBNKMWM")
-      url_new = URI("http://fallingfruit.org/api/0.2/clusters.json?#{rand_box}&api_key=BJBNKMWM")
-    end
+width = WHICH == 'locations' ? 0.1 : 2
+
+puts "old\tnew\tnewer"
+CSV.parse(File.read(CSV_PATH), headers: true, converters: :numeric).each do |row|
+  box = point_to_box(row['lat'], row['lng'], width)
+  old_bounds = box_to_old_bounds(box)
+  new_bounds = box_to_new_bounds(box)
+  if WHICH == 'clusters'
+    z = 13
+    # Clusters not supported in v0.1
+    url_old = URI("http://fallingfruit.org/api/locations/cluster.json")
+    # Uses 2^zoom+1, but with zoom only up to 12
+    url_new = URI("http://fallingfruit.org/api/0.2/clusters.json?#{old_bounds}&zoom=#{z - 1}&api_key=BJBNKMWM")
+    url_newer = URI("http://fallingfruit.org/api/0.3/clusters?#{new_bounds}&zoom=#{z}&api_key=BJBNKMWM")
   else
-    if Which == "cluster"
-      z = (12*rand).ceil
-      url_old = URI("http://fallingfruit.org/api/locations/cluster.json?#{rand_box}&grid=#{z}&api_key=BJBNKMWM")
-      url_new = URI("http://fallingfruit.org/api/0.2/clusters.json?#{rand_box}&zoom=#{z}&api_key=BJBNKMWM")
-    else
-      url_old = URI("http://fallingfruit.org/api/locations/markers.json?#{rand_box}&api_key=BJBNKMWM")
-      url_new = URI("http://fallingfruit.org/api/0.2/locations.json?#{rand_box}&api_key=BJBNKMWM")
-    end
+    # Muni off by default
+    url_old = URI("http://fallingfruit.org/api/locations/markers.json?#{old_bounds}&api_key=BJBNKMWM&muni=1")
+    url_new = URI("http://fallingfruit.org/api/0.2/locations.json?#{old_bounds}&api_key=BJBNKMWM")
+    url_newer = URI("http://fallingfruit.org/api/0.3/locations?#{new_bounds}&api_key=BJBNKMWM")
   end
-  $stderr.puts url_old
-  $stderr.puts url_new
+  unless DEBUG.nil?
+    $stderr.puts url_old
+    $stderr.puts url_new
+    $stderr.puts url_newer
+  end
   time_old = Benchmark.realtime{
     begin
       Net::HTTP.get(url_old)
@@ -74,5 +63,11 @@ end
     rescue Net::ReadTimeout
     end
   }
-  puts "#{time_old} #{time_new}"
-}
+  time_newer = Benchmark.realtime{
+    begin
+      Net::HTTP.get(url_newer)
+    rescue Net::ReadTimeout
+    end
+  }
+  puts "#{time_old}\t#{time_new}\t#{time_newer}"
+end
