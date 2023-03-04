df0d7430ac2a73fcfe728439287da25bad93eaef
ezwelty <ethan.welty@gmail.com>
2023-03-04 21:41:20 +0100

    Drop requirement for invasives data in migrations

--- db/migrate/20150920184333_add_invasive_field_to_locations.rb
+++ db/migrate/20150920184333_add_invasive_field_to_locations.rb
@@ -1,13 +1,10 @@
 class AddInvasiveFieldToLocations < ActiveRecord::Migration
   def up
-    prep = IO.read("util/invasives/prepare.sql")
-    execute prep
     add_column :locations, :invasive, :boolean, :default => false
-    execute "UPDATE locations SET invasive='t' FROM invasives i WHERE type_ids @> ARRAY[i.type_id] and ST_INTERSECTS(location,i.regions);"
+    execute "UPDATE locations SET invasive='t' FROM invasives i WHERE type_ids @> ARRAY[i.type_id] and ST_INTERSECTS(location, i.regions);"
   end
 
   def down
-    execute "DELETE FROM invasives WHERE 1=1;"
     remove_column :locations, :invaisve
   end
 end
